1dbccb4eef9805245a4bea3fcf43fa765a5e8a07
nathan samochval <samochval.nathan@gmail.com>
2023-02-04 23:57:57 +0100

    Renew

--- scripts/cisco.js
+++ scripts/cisco.js
@@ -59,49 +59,46 @@ function attendre(min, max) {
         }
       }
     } else {
-      const donne = await page.$$("#scrollableArea > div:nth-child(2) > div > main > div > section > div.__MR_HOME_SEARCH_MAIN_RESULTS__.flex-grow.flex.flex-col.justify-center.border-t.border-cc-gray-05.px-2.\\32 xs\\:px-0.py-6 > ul > li");
-      for (i = 1; i <= donne.length; i++) {
-        const suivant = await page.$('#scrollableArea > div:nth-child(2) > div > main > div > section > div.__MR_HOME_SEARCH_MAIN_RESULTS__.flex-grow.flex.flex-col.justify-center.border-t.border-cc-gray-05.px-2.\\32 xs\\:px-0.py-6 > ul > li:nth-child(' + i + ') > article');
-        await suivant.click();
-        await attendre(2000, 3000);
-        taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3');
-        if (await taille.length > 0) {
-          nom = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3').then(element => element[0].evaluate(node => node.textContent));
-        } else {
-          nom = '';
-        }
-        taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[1]');
-        if (await taille.length > 0) {
-          adresse = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[1]').then(element => element[0].evaluate(node => node.textContent));
-        } else {
-          adresse = '';
-        }
-        taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a');
-        if (await taille.length > 0) {
-          lien = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a').then(element => element[0].evaluate(node => node.textContent));
-        } else {
-          lien = '';
-        }
-        taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[2]/a');
-        if (await taille.length > 0) {
-          tel = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[2]/a').then(element => element[0].evaluate(node => node.textContent.replace(/,/g, '').replace('FRANCE', '')));
-        } else {
-          tel = '';
-        }
-        tab = [nom, adresse, tel, lien, 'Cisco'];
-        tab1 = tab.map(val => val.replace(/,/g, " ").replace('FRANCE', ' '));
-        csvData.push(tab1);
+      await page.goto(lien);
+      await attendre(10000, 10000)
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3');
+      if (await taille.length > 0) {
+        nom = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3').then(element => element[0].evaluate(node => node.textContent));
+      } else {
+        nom = '';
       }
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[1]');
+      if (await taille.length > 0) {
+        adresse = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[1]').then(element => element[0].evaluate(node => node.textContent));
+      } else {
+        adresse = '';
+      }
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a');
+      if (await taille.length > 0) {
+        site = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a').then(element => element[0].evaluate(node => node.textContent));
+      } else {
+        sites = '';
+      }
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[2]/a');
+      if (await taille.length > 0) {
+        tel = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[2]/a').then(element => element[0].evaluate(node => node.textContent.replace(/,/g, '').replace('FRANCE', '')));
+      } else {
+        tel = '';
+      }
+      tab = [nom, adresse, tel, site, 'Cisco'];
+      tab1 = tab.map(val => val.replace(/,/g, " ").replace('FRANCE', ' '));
+      console.log(tab1)
+      csvData.push(tab1);
     }
   });
-  await cluster.queue('https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/')
-  await cluster.idle();
-  first = false
-  const lienTotalPartner = Array.from(new Set(lienPartner));
-  for (j = 0; j < lienTotalPartner; j++) {
-    await cluster.queue(`https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/partner/${lienTotalPartner[j]}`)
-  }
-  await cluster.idle()
-  await cluster.close();
-  console.log(JSON.stringify(csvData))
-})();
+await cluster.queue('https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/')
+await cluster.idle();
+first = false
+const lienTotalPartner = Array.from(new Set(lienPartner));
+for (j = 0; j < lienTotalPartner.length; j++) {
+  await cluster.queue(`https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/partner/${lienTotalPartner[j]}`)
+}
+await cluster.idle()
+await cluster.close();
+console.log(JSON.stringify(csvData))
+}) ();
