d238a72d4e9fc964f7c2178567cff7e7efa488aa
nathan samochval <samochval.nathan@gmail.com>
2023-02-04 14:52:26 +0100

    NewCisco

--- scripts/cisco.js
+++ scripts/cisco.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const { JSDOM } = require('jsdom');
 
 let csvData = [];
-let arrayPartnerTotal = []
 let next = true
 let first = true
+let lienPartner = []
 // fonction pour faire un temps d'attente
 function attendre(min, max) {
   const temps = Math.random() * (max - min) + min;
@@ -18,36 +18,27 @@ function attendre(min, max) {
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
     maxConcurrency: 10,
-    timeout: 1000000000,
-    puppeteerOptions: {
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-      ]
-    },
+    timeout: 100000000,
   });
   await cluster.task(async ({ page, data: lien }) => {
     if (first) {
-      page.on('response', response => {
-        if (response.url().includes('getFavPartnersCount')) {
-          lienPartner = response._request._postData
-        }
-      });
       await page.goto(lien);
       await attendre(5000, 5000);
       const button = await page.$('#scrollableArea > div:nth-child(2) > div > main > div > header > div > div.mb-5 > div > div > button');
       await button.click();
       const select = await page.$('#scrollableArea > div:nth-child(2) > div > main > div > header > div > div.mb-5 > div > div > ul > li:nth-child(77) > span');
+      page.on('response', async response => {
+        if (response.url().includes('getPfPartners')) {
+          await response.json().then(data => {
+            for (let i = 0; i < 12; i++) {
+                lienPartner.push(data.data[i].site_id)
+            }
+          })
+        }
+      });
       await select.click();
-      await attendre(5000, 5000);
       while (next) {
         await attendre(10000, 10000)
-        const lienPartnerClean = lienPartner.replace('{"siteIdList":"', '').replace('"}', '')
-        const arrayPartner = lienPartnerClean.split(',')
-        for (i = 0; i < arrayPartner.length; i++) {
-          arrayPartnerTotal.push(arrayPartner[i])
-        }
         const pageSuivant = await page.$('#scrollableArea > div:nth-child(2) > div > main > div > section > div.__MR_HOME_SEARCH_MAIN_RESULTS__.flex-grow.flex.flex-col.justify-center.border-t.border-cc-gray-05.px-2.\\32 xs\\:px-0.py-6 > nav > ul > li:nth-child(3) > a > span');
         if (pageSuivant !== null) {
           await pageSuivant.click();
@@ -56,8 +47,11 @@ function attendre(min, max) {
         }
       }
     } else {
-      await page.goto(lien)
-      await attendre(10000,10000)
+      const donne = await page.$$("#scrollableArea > div:nth-child(2) > div > main > div > section > div.__MR_HOME_SEARCH_MAIN_RESULTS__.flex-grow.flex.flex-col.justify-center.border-t.border-cc-gray-05.px-2.\\32 xs\\:px-0.py-6 > ul > li");
+      for (i = 1; i <= donne.length; i++) {
+        const suivant = await page.$('#scrollableArea > div:nth-child(2) > div > main > div > section > div.__MR_HOME_SEARCH_MAIN_RESULTS__.flex-grow.flex.flex-col.justify-center.border-t.border-cc-gray-05.px-2.\\32 xs\\:px-0.py-6 > ul > li:nth-child(' + i + ') > article');
+        await suivant.click();
+        await attendre(2000, 3000);
         taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3');
         if (await taille.length > 0) {
           nom = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3').then(element => element[0].evaluate(node => node.textContent));
@@ -72,9 +66,9 @@ function attendre(min, max) {
         }
         taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a');
         if (await taille.length > 0) {
-          website = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a').then(element => element[0].evaluate(node => node.textContent));
+          lien = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a').then(element => element[0].evaluate(node => node.textContent));
         } else {
-          website = '';
+          lien = '';
         }
         taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[2]/a');
         if (await taille.length > 0) {
@@ -82,19 +76,20 @@ function attendre(min, max) {
         } else {
           tel = '';
         }
-        tab = [nom, adresse, tel, website, 'Cisco'];
+        tab = [nom, adresse, tel, lien, 'Cisco'];
         tab1 = tab.map(val => val.replace(/,/g, " ").replace('FRANCE', ' '));
         csvData.push(tab1);
       }
+    }
   });
   await cluster.queue('https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/')
   await cluster.idle();
   first = false
-  for (j = 0; j < arrayPartnerTotal; j++) {
-    await cluster.queue(`https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/partner/${arrayPartnerTotal[j]}`)
+  const lienTotalPartner = Array.from(new Set(lienPartner));
+  for (j = 0; j < lienTotalPartner; j++) {
+    await cluster.queue(`https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/partner/${lienTotalPartner[j]}`)
   }
   await cluster.idle()
   await cluster.close();
-  await attendre(5000,5000)
-  console.log(JSON.stringify(csvData))
+  cconsole.log(JSON.stringify(csvData))
 })();
