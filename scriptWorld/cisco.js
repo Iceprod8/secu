aca698507f995992db2a3736169b2af54c1edcd0
nathan samochval <samochval.nathan@gmail.com>
2023-02-06 10:21:22 +0100

    SecuApp

--- scriptWorld/cisco.js
+++ scriptWorld/cisco.js
@@ -2,10 +2,11 @@ const { Cluster } = require('puppeteer-cluster');
 const fs = require('fs');
 const { JSDOM } = require('jsdom');
 
-
-
 let csvData = [];
-csvData.push(['']);
+let next = true
+let first = true
+let lienPartner = []
+let error = []
 // fonction pour faire un temps d'attente
 function attendre(min, max) {
   const temps = Math.random() * (max - min) + min;
@@ -13,23 +14,85 @@ function attendre(min, max) {
     setTimeout(resolve, temps);
   });
 }
+
 (async () => {
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 1,
+    maxConcurrency: 5,
+    timeout: 100000000,
     puppeteerOptions: {
-      headless: false,
-      executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ]
     },
   });
-  await cluster.task(async ({ page, data: url }) => {
-    await page.goto(url);
-    await attendre(5000,5000)
-    page.on('response', response => {
-      console.log(response.url());
-    });
-    });
+  await cluster.task(async ({ page, data: lien }) => {
+    if (first) {
+      await page.goto(lien);
+      page.on('response', async response => {
+        if (response.url().includes('getPfPartners')) {
+          await response.json().then(data => {
+            for (let i = 0; i < 12; i++) {
+              try {
+                lienPartner.push(data.data[i].site_id)
+              } catch (err) {
+                error.push(err)
+              }
+            }
+          })
+        }
+      });
+      while (next) {
+        await attendre(10000, 10000)
+        const pageSuivant = await page.$('#scrollableArea > div:nth-child(2) > div > main > div > section > div.__MR_HOME_SEARCH_MAIN_RESULTS__.flex-grow.flex.flex-col.justify-center.border-t.border-cc-gray-05.px-2.\\32 xs\\:px-0.py-6 > nav > ul > li:nth-child(3) > a > span');
+        if (pageSuivant !== null) {
+          await pageSuivant.click();
+        } else {
+          next = false;
+        }
+      }
+    } else {
+      await page.goto(lien);
+      await attendre(10000, 10000)
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3');
+      if (await taille.length > 0) {
+        nom = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/h3').then(element => element[0].evaluate(node => node.textContent));
+      } else {
+        nom = '';
+      }
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[1]');
+      if (await taille.length > 0) {
+        adresse = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[1]').then(element => element[0].evaluate(node => node.textContent));
+      } else {
+        adresse = '';
+      }
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a');
+      if (await taille.length > 0) {
+        site = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[1]/a').then(element => element[0].evaluate(node => node.textContent));
+      } else {
+        sites = '';
+      }
+      taille = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[2]/a');
+      if (await taille.length > 0) {
+        tel = await page.$x('//*[@id="scrollableArea"]/div[1]/div[1]/div[1]/section/div[2]/p[2]/a').then(element => element[0].evaluate(node => node.textContent.replace(/,/g, '').replace('FRANCE', '')));
+      } else {
+        tel = '';
+      }
+      tab = [nom, adresse, tel, site, 'Cisco'];
+      tab1 = tab.map(val => val.replace(/,/g, " ").replace('FRANCE', ' '));
+      csvData.push(tab1);
+    }
+  });
 await cluster.queue('https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/')
 await cluster.idle();
+first = false
+const lienTotalPartner = Array.from(new Set(lienPartner));
+for (j = 0; j < lienTotalPartner.length; j++) {
+  await cluster.queue(`https://locatr.cloudapps.cisco.com/WWChannels/LOCATR/pf/index.jsp#/partner/${lienTotalPartner[j]}`)
+}
+await cluster.idle()
 await cluster.close();
+console.log(JSON.stringify(csvData))
 }) ();
